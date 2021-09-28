30f3ab12ad6a4924c56e2e8ca950a0cb5d920887
Victory Omole <vtomole2@gmail.com>
2021-09-28 16:32:21 -0500

    Update cirq_superstaq/service.py
    Co-authored-by: richrines1 <85512171+richrines1@users.noreply.github.com>

--- cirq_superstaq/service.py
+++ cirq_superstaq/service.py
@@ -44,7 +44,7 @@ def counts_to_results(
     """
     measurement_key_names = (
         list(circuit.all_measurement_keys())
-        if "0.13.0" not in cirq.__version__
+        if not cirq.__version__.startswith("0.13.0")
         else list(circuit.all_measurement_key_names())  # type: ignore
     )
 
