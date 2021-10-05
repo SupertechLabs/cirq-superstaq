6e6e4647e5bf1db90872dc8410ef8cc1c7737ac1
David Owusu-Antwi <david@super.tech>
2021-10-05 18:47:34 -0500

    reworked yaml file, removed old shell script

--- .github/workflows/update-version-tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This updates the version number by accessing the most recent tagged commit.
-echo "__version__ =" \"$(git describe --tags --abbrev=0 | cut -c2-)\" > 'cirq-superstaq/_version.py'
