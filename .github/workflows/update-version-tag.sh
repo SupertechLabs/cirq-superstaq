bc61508d9d259ae1f4168720df04dcda73b901e1
David Owusu-Antwi <david@super.tech>
2021-10-05 17:45:02 -0500

    rewrote shell script to fix syntax

--- .github/workflows/update-version-tag.sh
+++ .github/workflows/update-version-tag.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # This updates the version number by accessing the most recent tagged commit.
-VERSION_TAG=$(git describe --tags --abbrev=0)
-echo \" $VERSION_TAG | cut -c2- \" > 'cirq-superstaq/_version.py'
+echo "__version__ =" \"$(git describe --tags --abbrev=0 | cut -c2-)\" > 'cirq-superstaq/_version.py'
