e9f1e97f2bc1857aca5b4713fc7e00476240c169
David Owusu-Antwi <david@super.tech>
2021-11-01 13:22:13 -0500

    added circuit output, edited doc to reflect actual output

--- examples/cirq_superstaq_example.py
+++ examples/cirq_superstaq_example.py
@@ -7,7 +7,7 @@ Circuit:
 1: ----------X-----------------
 
 Results:
-output=1100011010111001...1101 (i.e., `job.counts()` returns {'0': 45, '1': 55})
+{'0': 45, '1': 55}
 """
 
 import cirq
@@ -25,6 +25,7 @@ service = cirq_superstaq.Service(
 q0 = cirq.LineQubit(0)
 q1 = cirq.LineQubit(1)
 circuit = cirq.Circuit(cirq.H(q0), cirq.CNOT(q0, q1), cirq.measure(q0))
+print(circuit)
 
 # Creating job with Service object.
 job = service.create_job(circuit=circuit, repetitions=100, target="ibmq_qasm_simulator")
