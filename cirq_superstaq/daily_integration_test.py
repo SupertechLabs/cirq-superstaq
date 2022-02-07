70e1529d2c174385fd6a749f08ef0e4726a934fb
David Owusu-Antwi <david@super.tech>
2022-02-07 11:37:33 -0600

    nit: single line measurement

--- cirq_superstaq/daily_integration_test.py
+++ cirq_superstaq/daily_integration_test.py
@@ -100,8 +100,7 @@ def test_cq_compile(service: cirq_superstaq.Service) -> None:
     circuit = cirq.Circuit(
         cirq.H(qubits[0]),
         cirq.CNOT(qubits[0], qubits[1]),
-        cirq.measure(qubits[0]),
-        cirq.measure(qubits[1]),
+        cirq.measure(qubits[0], qubits[1]),
     )
     compiled_circuit = cirq.Circuit(
         cirq_superstaq.ParallelRGate(-0.25 * np.pi, 0.5 * np.pi, 2).on(qubits[0], qubits[1]),
