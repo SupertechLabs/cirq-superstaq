9742d44df96274288b538a6ba035f8dae7ea1a7d
David Owusu-Antwi <david@super.tech>
2022-02-10 11:19:55 -0600

    adds parameter to service.run method

--- cirq_superstaq/service.py
+++ cirq_superstaq/service.py
@@ -167,6 +167,7 @@ class Service(finance.Finance, logistics.Logistics, user_config.UserConfig):
         circuit: cirq.Circuit,
         repetitions: int,
         target: Optional[str] = None,
+        ibmq_pulse: Optional[bool] = None,
         param_resolver: cirq.ParamResolver = cirq.ParamResolver({}),
     ) -> cirq.Result:
         """Run the given circuit on the SuperstaQ API and returns the result
@@ -176,12 +177,13 @@ class Service(finance.Finance, logistics.Logistics, user_config.UserConfig):
             circuit: The circuit to run.
             repetitions: The number of times to run the circuit.
             target: Where to run the job. Can be 'qpu' or 'simulator'.
+            ibmq_pulse: Specify whether to run the job using SuperstaQ's pulse-level optimizations.
             param_resolver: A `cirq.ParamResolver` to resolve parameters in  `circuit`.
 
         Returns:
             A `cirq.Result` for running the circuit.
         """
-        counts = self.get_counts(circuit, repetitions, target, param_resolver)
+        counts = self.get_counts(circuit, repetitions, target, ibmq_pulse, param_resolver)
         return counts_to_results(counts, circuit, param_resolver)
 
     def sampler(self, target: str) -> cirq.Sampler:
