8fff3e2af0e4e0a653c4714dace97d1fa69323de
richrines1 <85512171+richrines1@users.noreply.github.com>
2022-04-20 20:47:25 +0000

    Update cirq_superstaq/service.py
    Co-authored-by: Pranav Gokhale <pranav@super.tech>

--- cirq_superstaq/service.py
+++ cirq_superstaq/service.py
@@ -296,6 +296,8 @@ class Service(finance.Finance, logistics.Logistics, user_config.UserConfig):
         target: str = "keysight",
     ) -> "cirq_superstaq.compiler_output.CompilerOutput":
         """Compiles the given circuit to target AQT device with Equivalent Circuit Averaging (ECA).
+        
+        See arxiv.org/pdf/2111.04572.pdf for a description of ECA.
 
         Args:
             circuit: cirq Circuit to compile.
