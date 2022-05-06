6d1b71e67d30bc498eb7783b8a6aa771afd9f949
vtomole <vtomole2@gmail.com>
2022-05-06 15:34:23 -0500

    Remove test

--- cirq_superstaq/daily_integration_test.py
+++ cirq_superstaq/daily_integration_test.py
@@ -128,17 +128,6 @@ def test_cq_compile(service: cirq_superstaq.Service) -> None:
     circuit = cirq.Circuit(
         cirq.H(qubits[0]), cirq.CNOT(qubits[0], qubits[1]), cirq.measure(qubits[0])
     )
-    compiled_circuit = cirq.Circuit(
-        cirq_superstaq.ParallelRGate(-0.25 * np.pi, 0.5 * np.pi, 2).on(qubits[0], qubits[1]),
-        cirq.Z(qubits[0]) ** -1.0,
-        cirq.Z(qubits[1]) ** -1.0,
-        cirq_superstaq.ParallelRGate(0.25 * np.pi, 0.5 * np.pi, 2).on(qubits[0], qubits[1]),
-        cirq.CZ(qubits[0], qubits[1]) ** -1.0,
-        cirq_superstaq.ParallelRGate(-0.25 * np.pi, 0.5 * np.pi, 2).on(qubits[0], qubits[1]),
-        cirq.Z(qubits[1]) ** -1.0,
-        cirq_superstaq.ParallelRGate(0.25 * np.pi, 0.5 * np.pi, 2).on(qubits[0], qubits[1]),
-        cirq.measure(qubits[0]),
-    )
 
     out = service.cq_compile(circuit)
     cirq.testing.assert_circuits_with_terminal_measurements_are_equivalent(
